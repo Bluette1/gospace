f8d0c9b668a1d2d52edf31da4089deaac8931089
Bluette1 <marylene.sawyer@gmail.com>
2021-06-14 13:28:55 +0200

    Fundamentals go: Setting project repo structure

---
+++ src/github.com/bluette1/firstapp/Main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, Go!")
+}
+
+// Build command:
+// Use package path not folder path
+// go build <package path>
+// go build github.com/bluette1/firstapp/
+
+// Install command: - installs binary executable in bin folder
+// Use package path not folder path
+// go install <package path>
