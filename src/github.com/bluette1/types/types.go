d937c9dd19a15bd7e3e8d0ce93946233e17a3dd5
Bluette1 <marylene.sawyer@gmail.com>
2021-07-02 17:46:11 +0200

    Add numeral, string types

--- src/github.com/bluette1/types/types.go
+++ src/github.com/bluette1/types/types.go
@@ -19,4 +19,38 @@ func main() {
 
 	 fmt.Printf("%v , %T\n", n, n)
 	 fmt.Printf("%v , %T\n", m, m)
+
+	 //numerical types
+	 //signed integers
+	 //unsigned integers: store only bigger positives(no negatives)
+	 var a uint16 = 13
+	 fmt.Printf("%v , %T\n", a, a)
+
+	 //Bitwise operatios
+	//  - or, and, xor
+
+	 //Bit shifting
+	 b := 8 // 2^3
+	 fmt.Println(b << 3)
+	 fmt.Println(b >> 3)
+
+	 //floating point numbers
+	 //float32 float64
+
+	 //Complex numbers exist 
+	 c := complex(5, 12) //complex128
+	 fmt.Printf("%v , %T\n",c, c)
+
+	 s := "this is a string"
+	 fmt.Printf("%v , %T\n",s[2], s[2]) // the string type is an alias for byte, 
+	 // ASII/utf-8 encoding
+	 //105, []unint8
+	 fmt.Printf("%v , %T\n",string(s[2]), s[2])
+
+	 byteS := []byte(s)
+	 fmt.Printf("%v , %T\n", byteS, byteS)
+
+
+	 //type rune int32
+	 //Default types are fine most of the time
 }
