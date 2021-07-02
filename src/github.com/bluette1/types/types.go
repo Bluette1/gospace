69684e8fc66b4b36b5c89a01daf5e363d7f14dc2
Bluette1 <marylene.sawyer@gmail.com>
2021-07-02 17:04:55 +0200

    Add boolean types

--- src/github.com/bluette1/types/types.go
+++ src/github.com/bluette1/types/types.go
@@ -9,5 +9,14 @@ func main() {
 	var i int = 0
 	var j string
 	j = strconv.Itoa(i)
-	fmt.Printf("%v, %T", j, j)
+	fmt.Printf("%v , %T\n", j, j)
+
+	//boolean type
+	var n bool //zero value is false
+	fmt.Printf("%v , %T\n", n, n)
+	 n = 1 == 2
+	 m := 2 == 2
+
+	 fmt.Printf("%v , %T\n", n, n)
+	 fmt.Printf("%v , %T\n", m, m)
 }
