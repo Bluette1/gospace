5a6ab7366a00e55a3791d54d64e4bf97276eac1a
Bluette1 <marylene.sawyer@gmail.com>
2021-07-03 07:56:53 +0200

    Add CrcCheck function

--- src/github.com/bluette1/holabot/Main.go
+++ src/github.com/bluette1/holabot/Main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+    "os"
     "fmt"
     "log"
     "github.com/gorilla/mux"
     "github.com/joho/godotenv"
     "net/http"
+    "crypto/hmac"
+    "crypto/sha256"
+    "encoding/base64"
+    "encoding/json"
 )
 
 func main(){
@@ -36,5 +41,23 @@ func main(){
 }
 
 func CrcCheck(writer http.ResponseWriter, request *http.Request){
-    //TODO implement CRC check
+    //Set response header to json type
+    writer.Header().Set("Content-Type", "application/json")
+    //Get crc token in parameter
+    token := request.URL.Query()["crc_token"]
+    if len(token) < 1 {
+        fmt.Fprintf(writer,"No crc_token given")
+        return
+    }
+
+    //Encrypt and encode in base 64 then return
+    h := hmac.New(sha256.New, []byte(os.Getenv("CONSUMER_SECRET")))
+    h.Write([]byte(token[0]))
+    encoded := base64.StdEncoding.EncodeToString(h.Sum(nil))
+    //Generate response string map
+    response := make(map[string]string)
+    response["response_token"] =  "sha256=" + encoded
+    //Turn response map to json and send it to the writer
+    responseJson, _ := json.Marshal(response)
+    fmt.Fprintf(writer, string(responseJson))
 }
