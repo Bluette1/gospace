1b633dc3934f90f56b123e3917faf2f53bbaaa1b
Bluette1 <marylene.sawyer@gmail.com>
2021-07-03 07:37:58 +0200

    Add .env file to correct path

--- src/github.com/bluette1/holabot/Main.go
+++ src/github.com/bluette1/holabot/Main.go
@@ -2,45 +2,7 @@ package main
 
 import (
     "fmt"
-    "github.com/gorilla/mux"
-    "github.com/joho/godotenv"
-    "net/http"
-)
-
-func main(){
-    //Load env
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file")
-        fmt.Println("Error loading .env file")
-    }
-    fmt.Println("Starting Server")
-
-    //Create a new Mux Handler
-    m := mux.NewRouter()
-    //Listen to the base url and send a response
-    m.HandleFunc("/", func(writer http.ResponseWriter, _ *http.Request) {
-        writer.WriteHeader(200)
-        fmt.Fprintf(writer, "Server is up and running")
-    })
-    //Listen to crc check and handle
-    m.HandleFunc("/webhook/twitter", CrcCheck).Methods("GET")
-
-    //Start Server
-    server := &http.Server{
-        Handler: m,
-    }
-    server.Addr = ":9090"
-    server.ListenAndServe()
-}
-
-func CrcCheck(writer http.ResponseWriter, request *http.Request){
-    //TODO implement CRC check
-}
-package main
-
-import (
-    "fmt"
+    "log"
     "github.com/gorilla/mux"
     "github.com/joho/godotenv"
     "net/http"
