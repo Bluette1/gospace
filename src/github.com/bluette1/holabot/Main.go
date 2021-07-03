c4c93ec2f12accaf694f7a93bf95c89a77fb8043
Bluette1 <marylene.sawyer@gmail.com>
2021-07-04 00:08:12 +0200

    Register the webhook

--- src/github.com/bluette1/holabot/Main.go
+++ src/github.com/bluette1/holabot/Main.go
@@ -3,10 +3,14 @@ package main
 import (
     "os"
     "fmt"
+    "bytes"
     "log"
+    "io/ioutil"
     "github.com/gorilla/mux"
     "github.com/joho/godotenv"
+    "github.com/dghubble/oauth1"
     "net/http"
+    "net/url"
     "crypto/hmac"
     "crypto/sha256"
     "encoding/base64"
@@ -21,6 +25,11 @@ func main(){
         fmt.Println("Error loading .env file")
     }
     fmt.Println("Starting Server")
+    
+    //Check for -register in agument list
+    if args := os.Args; len(args) > 1 && args[1] == "-register"{
+        go registerWebhook()
+    }
 
     //Create a new Mux Handler
     m := mux.NewRouter()
@@ -60,4 +69,50 @@ func CrcCheck(writer http.ResponseWriter, request *http.Request){
     //Turn response map to json and send it to the writer
     responseJson, _ := json.Marshal(response)
     fmt.Fprintf(writer, string(responseJson))
+}
+
+func CreateClient() *http.Client {
+    //Create oauth client with consumer keys and access token
+    config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET"))
+    token := oauth1.NewToken(os.Getenv("ACCESS_TOKEN_KEY"), os.Getenv("ACCESS_TOKEN_SECRET"))
+    // subscribeWebhook()
+
+    return config.Client(oauth1.NoContext, token)
+}
+func registerWebhook(){
+    fmt.Println("Registering webhook...")
+    httpClient := CreateClient()
+
+    //Set parameters
+    path := "https://api.twitter.com/1.1/account_activity/webhooks.json"
+
+    values := url.Values{}
+    values.Set("url", os.Getenv("APP_URL")+"/webhook/twitter")
+
+    //Make Oauth Post with parameters
+    resp, _ := httpClient.PostForm(path, values)
+    defer resp.Body.Close()
+    //Parse response and check response
+    body, _ := ioutil.ReadAll(resp.Body)
+    var data map[string]interface{}
+    if err := json.Unmarshal([]byte(body), &data); err != nil {
+        panic(err)
+    }
+    fmt.Println("Webhook id of " + data["id"].(string) + " has been registered")
+}
+
+func subscribeWebhook(){
+    fmt.Println("Subscribing webapp...")
+    client := CreateClient()
+    path := "https://api.twitter.com/1.1/account_activity/all/" + os.Getenv("WEBHOOK_ENV") + "/subscriptions.json"
+    resp, _ := client.PostForm(path, nil)
+    body, _ := ioutil.ReadAll(resp.Body)
+    defer resp.Body.Close()
+    //If response code is 204 it was successful
+    if resp.StatusCode == 204 {
+        fmt.Println("Subscribed successfully")
+    } else if resp.StatusCode!= 204 {
+        fmt.Println("Could not subscribe the webhook. Response below:")
+        fmt.Println(string(body))
+    }
 }
