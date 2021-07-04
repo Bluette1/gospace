34ef2a755daec58b6034ce891ea6b17a83473d37
Bluette1 <marylene.sawyer@gmail.com>
2021-07-04 08:43:22 +0200

    Registering the twitter webhook

--- src/github.com/bluette1/holabot/Main.go
+++ src/github.com/bluette1/holabot/Main.go
@@ -22,7 +22,70 @@ import (
 	"net/http"
 	"os"
 	"strings"
+	"log"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"net/url"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
 )
+
+func main(){
+	// Load env
+	err := godotenv.Load()
+	if err != nil {
+			log.Fatal("Error loading .env file")
+			fmt.Println("Error loading .env file")
+	}
+	fmt.Println("Starting Server")
+	
+	//Check for -register in agument list
+	if args := os.Args; len(args) > 1 && args[1] == "-register"{
+			go registerWebhook()
+	}
+
+	//Create a new Mux Handler
+	m := mux.NewRouter()
+	//Listen to the base url and send a response
+	m.HandleFunc("/", func(writer http.ResponseWriter, _ *http.Request) {
+			writer.WriteHeader(200)
+			fmt.Fprintf(writer, "Server is up and running")
+	})
+	//Listen to crc check and handle
+	m.HandleFunc("/webhook/twitter", CrcCheck).Methods("GET")
+
+	//Start Server
+	server := &http.Server{
+			Handler: m,
+	}
+	server.Addr = ":9090"
+	server.ListenAndServe()
+}
+func CrcCheck(writer http.ResponseWriter, request *http.Request){
+	//Set response header to json type
+	writer.Header().Set("Content-Type", "application/json")
+	//Get crc token in parameter
+	token := request.URL.Query()["crc_token"]
+	if len(token) < 1 {
+			fmt.Fprintf(writer,"No crc_token given")
+			return
+	}
+
+	//Encrypt and encode in base 64 then return
+	h := hmac.New(sha256.New, []byte(os.Getenv("CONSUMER_SECRET")))
+	h.Write([]byte(token[0]))
+	encoded := base64.StdEncoding.EncodeToString(h.Sum(nil))
+	//Generate response string map
+	response := make(map[string]string)
+	response["response_token"] =  "sha256=" + encoded
+	//Turn response map to json and send it to the writer
+	responseJson, _ := json.Marshal(response)
+	fmt.Println(string(responseJson))
+	fmt.Fprintf(writer, string(responseJson))
+}
+
 
 func LoadCredentials() (client *twittergo.Client, err error) {
 	credentials, err := ioutil.ReadFile("CREDENTIALS")
@@ -38,43 +101,46 @@ func LoadCredentials() (client *twittergo.Client, err error) {
 	client = twittergo.NewClient(config, user)
 	return
 }
-
-func main() {
+func registerWebhook(){
+	fmt.Println("Registering webhook...")
+	// httpClient := CreateClient()
 	var (
 		err    error
 		client *twittergo.Client
 		req    *http.Request
 		resp   *twittergo.APIResponse
-		user   *twittergo.User
+		// user   *twittergo.User
 	)
 	client, err = LoadCredentials()
 	if err != nil {
 		fmt.Printf("Could not parse CREDENTIALS file: %v\n", err)
 		os.Exit(1)
 	}
-	req, err = http.NewRequest("GET", "/1.1/account/verify_credentials.json", nil)
+
+	//Set parameters
+	path := "/1.1/account_activity/all/"+os.Getenv("WEBHOOK_ENV")+"/webhooks.json"
+  fmt.Println(path)
+	values := url.Values{}
+	appUrl := url.QueryEscape(os.Getenv("APP_URL")+"/webhook/twitter")
+	values.Set("url", appUrl)
+
+	body := strings.NewReader(values.Encode())
+	req, err = http.NewRequest("POST", path, body)
+	defer req.Body.Close()
+	
 	if err != nil {
 		fmt.Printf("Could not parse request: %v\n", err)
 		os.Exit(1)
 	}
+
 	resp, err = client.SendRequest(req)
 	if err != nil {
 		fmt.Printf("Could not send request: %v\n", err)
 		os.Exit(1)
 	}
-	user = &twittergo.User{}
-	err = resp.Parse(user)
-	if err != nil {
-		fmt.Printf("Problem parsing response: %v\n", err)
-		os.Exit(1)
-	}
-	fmt.Printf("ID:                   %v\n", user.Id())
-	fmt.Printf("Name:                 %v\n", user.Name())
-	if resp.HasRateLimit() {
-		fmt.Printf("Rate limit:           %v\n", resp.RateLimit())
-		fmt.Printf("Rate limit remaining: %v\n", resp.RateLimitRemaining())
-		fmt.Printf("Rate limit reset:     %v\n", resp.RateLimitReset())
-	} else {
-		fmt.Printf("Could not parse rate limit from response.\n")
-	}
+
+	// Parse response and check response
+	respBody, err := ioutil.ReadAll(resp.Body)
+
+    fmt.Println(string(respBody))
 }
