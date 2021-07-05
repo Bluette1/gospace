ec454163f7995a0d9346c21001078d6e22959f95
Bluette1 <marylene.sawyer@gmail.com>
2021-07-05 08:19:32 +0200

    Add constants

--- src/github.com/bluette1/constants/Main.go
+++ src/github.com/bluette1/constants/Main.go
@@ -3,8 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	const myConst = 42
+	const a = 42
 
-	// const myConst int = 42
-	fmt.Printf("%v, %T\n", myConst, myConst)
+	// const a int = 42
+	fmt.Printf("%v, %T\n", a, a)
+
+	//Inferring the type
+	var b int16 = 4
+	fmt.Printf("%v, %T\n", a+b, a+b)
+  //Enumerated constants
+	//Variable block
+	// const (
+	// e = iota
+	// d = iota
+	// c = iota
+	// )
+
+	const (
+		e = iota
+		d
+		c
+		)
+	fmt.Println(e)
+	fmt.Println(d)
+	fmt.Println(c)
+
+  //Resets iota
+	const (
+		_ = iota //Right-only variable(we are not going to use it)
+	  f
+		g
+		)
+		fmt.Println(d)
+		fmt.Println(c)
 }
